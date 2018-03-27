f6c5aca7733f2fa5b8df69d2479e20b7584dc65d
Steve Lane <lane.s@unimelb.edu.au>
2018-03-27 14:16:21 +1100

    repeat command for tweeting

--- R/createTweets.R
+++ R/createTweets.R
@@ -4,7 +4,7 @@
 ## Author: Steve Lane
 ## Date: Tuesday, 27 March 2018
 ## Synopsis: Creates tweets for twitterbot
-## Time-stamp: <2018-03-27 13:59:14 (slane)>
+## Time-stamp: <2018-03-27 14:15:57 (slane)>
 ################################################################################
 ################################################################################
 library(dplyr)
@@ -13,6 +13,16 @@ library(rtweet)
 source("getLists.R")
 source("getImage.R")
 source("tweetImage.R")
+
+## Grab all pests
 all_pests <- getLists()
-one_pest <- getImage(all_pests)
-tweetImage(one_pest)
+
+## Send a tweet every hour with a randomly selected pest (may get repeats!)
+repeat({
+    ## Sample a pest
+    one_pest <- getImage(all_pests)
+    ## Tweet
+    tweetImage(one_pest)
+    ## Go to sleep for 15 minutes
+    Sys.sleep(60 * 15)
+})
